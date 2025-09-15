034d939011adae42931f9e57a9501a9a9834cfdc
Kanak Tiwari <147815832+Kanakt326@users.noreply.github.com>
2025-09-15 22:11:21 +0530

    Update and rename p.py to tocheckkeymodel.py

--- tocheckkeymodel.py
+++ tocheckkeymodel.py
@@ -1,9 +1,10 @@
 import google.generativeai as genai
 
 # Use your key safely
-genai.configure(api_key="AIzaSyB2IddMajaRCJ9U4SBIQ_hs-kE7gdp7hSk")  # replace YOUR_NEW_KEY with the regenerated key
+genai.configure(api_key="Yourapikey")  # replace YOUR_NEW_KEY with the regenerated key
 
 # List available models
 models = genai.list_models()
 for m in models:
     print(m.name, m.description)
+
